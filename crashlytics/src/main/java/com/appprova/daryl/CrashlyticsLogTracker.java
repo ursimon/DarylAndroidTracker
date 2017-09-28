1f0a3e9a4a934457742edbce7ae923646749479c
Michal Ursiny <michal.ursiny@gmail.com>
2017-09-28 22:36:59 +0200

    Crashlytics log tracker using Answers instead

--- crashlytics/src/main/java/com/appprova/daryl/CrashlyticsLogTracker.java
+++ crashlytics/src/main/java/com/appprova/daryl/CrashlyticsLogTracker.java
@@ -2,22 +2,47 @@ package com.appprova.daryl;
 
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.ContentViewEvent;
+import com.crashlytics.android.answers.CustomEvent;
 
 import java.util.Map;
 
 public class CrashlyticsLogTracker implements TrackerAdapter {
     @Override
     public void logPageView(String name) {
-        Crashlytics.log("pageView: " + name);
+        Answers.getInstance().logContentView(new ContentViewEvent().putContentName(name));
     }
 
     @Override
     public void logEvent(Map<String, Object> eventData) {
-        Crashlytics.log(EventToStringConverter.toString(eventData));
+        final String name = (String) eventData.get(Constants.EVENT_NAME);
+        CustomEvent customEvent = new CustomEvent(name);
+        final String category = (String) eventData.get(Constants.EVENT_CATEGORY);
+        if (category != null) {
+            customEvent.putCustomAttribute(Constants.EVENT_CATEGORY, category);
+        }
+        final String action = (String) eventData.get(Constants.EVENT_ACTION);
+        if (action != null) {
+            customEvent.putCustomAttribute(Constants.EVENT_ACTION, action);
+        }
+        final String label = (String) eventData.get(Constants.EVENT_LABEL);
+        if (label != null) {
+            customEvent.putCustomAttribute(Constants.EVENT_LABEL, label);
+        }
+        final String campaignUrl = (String) eventData.get(Constants.EVENT_CAMPAIGN_URL);
+        if (campaignUrl != null) {
+            customEvent.putCustomAttribute(Constants.EVENT_CAMPAIGN_URL, campaignUrl);
+        }
+        final Long value = (Long) eventData.get(Constants.EVENT_VALUE);
+        if (value != null) {
+            customEvent.putCustomAttribute(Constants.EVENT_VALUE, value);
+        }
+        Answers.getInstance().logCustom(customEvent);
     }
 
     @Override
-        public void setUserProperty(String key, Object value) {
+    public void setUserProperty(String key, Object value) {
         switch (key) {
             case Constants.USER_PROPERTY_EMAIL:
                 Crashlytics.setUserEmail(value.toString());
